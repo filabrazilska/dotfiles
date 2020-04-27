39d0d5ce047917690b89c4322404a2f2d650aab9
Filip Andres <filip@andresovi.net>
2020-04-27 12:33:44 +0200

    Add some small helper tools

---
+++ bin/check_battery.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LIMIT=15
+BAT=$(/usr/bin/upower -e | grep BAT)
+POW=$(/usr/bin/upower -i $BAT | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+
+# DBUS_SESSION_FILE=~/.dbus/session-bus/$(cat /var/lib/dbus/machine-id)-0
+# if [ -e "$DBUS_SESSION_FILE" ]; then
+#     . "$DBUS_SESSION_FILE"
+#     export DBUS_SESSION_BUS_ADDRESS DBUS_SESSION_BUS_PID
+# fi
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
+if (($POW < $LIMIT)); then
+    /usr/bin/notify-send -u critical -t 15000 "Low battery: $POW%"
+fi
