357a37a312ca58527ab90fc80671ce2bdcab3abd
Filip Andres <filip@andresovi.net>
2020-05-11 11:19:42 +0200

    Do not send notification when already charging

--- bin/check_battery.sh
+++ bin/check_battery.sh
@@ -3,6 +3,7 @@
 LIMIT=15
 BAT=$(/usr/bin/upower -e | grep BAT)
 POW=$(/usr/bin/upower -i $BAT | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+CHARGING=$(/usr/bin/upower -i $BAT | grep -q 'state:\s*charging' && echo 1 || echo 0)
 
 # DBUS_SESSION_FILE=~/.dbus/session-bus/$(cat /var/lib/dbus/machine-id)-0
 # if [ -e "$DBUS_SESSION_FILE" ]; then
@@ -11,6 +12,7 @@ POW=$(/usr/bin/upower -i $BAT | grep --color=never -E percentage|xargs|cut -d' '
 # fi
 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
-if (($POW < $LIMIT)); then
+
+if [[ "$CHARGING" -eq "0" && (($POW < $LIMIT)) ]]; then
     /usr/bin/notify-send -u critical -t 15000 "Low battery: $POW%"
 fi
